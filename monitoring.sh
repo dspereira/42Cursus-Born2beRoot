e58d954c292e709bfc93bcb1bc04c2bdf96c5519
Diogo Silverio pereira <dsilveri@c1r13s4.42lisboa.com>
2021-12-10 17:04:57 +0000

    update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -11,22 +11,21 @@ export USAGE_MEM_PERCENT=$(free -m | grep -m 1 "Mem" | awk '{printf "%0.2f", $3/
 export ROOT_MEM=$(df -Bg | grep "root" | awk '{print $2+0}')
 export HOME_MEM=$(df -Bg | grep "home" | awk '{print $2+0}')
 export FULL_DISK=$(echo $(($HOME_MEM+$ROOT_MEM)))
+
 ROOT_MEM=$(df -Bm | grep "root" | awk '{print $3+0}')
 HOME_MEM=$(df -Bm | grep "home" | awk '{print $3+0}')
 export USED_DISK=$(echo $(($HOME_MEM+$ROOT_MEM)))
-ROOT_MEM=$(df | grep "root" | awk '{print $5+0}')
-HOME_MEM=$(df | grep "home" | awk '{print $5+0}')
-export USED_DISK_PERCENT=$(echo $(($HOME_MEM+$ROOT_MEM)))
+
+export ROOT_MEM=$(df -Bm | grep "root" | awk '{print $2+0}')
+export HOME_MEM=$(df -Bm | grep "home" | awk '{print $2+0}')
+export FULL_DISK_MB=$(echo $(($HOME_MEM+$ROOT_MEM)))
+export USED_DISK_PERCENT=$(echo "$USED_DISK $FULL_DISK_MB" | awk '{printf "%0.2f", $1/$2*100}')
+
 
 export CPU_LOAD=$(top -bn 1 | grep "Cpu(s)" | awk '{printf "%0.1f", $2+$4 }')
-
-#PARA DEBIAN EM INGLÊS print $3, $4
-#export UPTIME=$(who -b | awk '{print $3, $4}')
-export UPTIME=$(who -b | awk '{print $4, $5}')
-
+export UPTIME=$(who -b | awk '{print $3, $4}')
 export LVM=$(lsblk | grep -m 1 lvm | wc -l)
-# No Debian tem de ser if [ $LVM = 1 ] com um '=' e não 2
-if [ $LVM == 1 ]
+if [ $LVM > 0 ]
 then
    LVM="yes"
 else
@@ -35,16 +34,11 @@ fi
 
 export TCP=$(ss -at | grep ESTAB | wc -l)
 export LOGED_USERS=$(who -u | wc -l)
-#colocar hostname -I não existe no manjaro
-#export IP=$(hostname -I) 
-export IP=$(hostname -i) 
+export IP=$(hostname -I) 
 export MAC=$(ip address | grep -m 1 "link/ether" | awk '{print $2}')
-#export SUDO=$(journalctl -q | grep sudo | grep COMMAND | wc -l)
-export SUDO=10
+export SUDO=$(journalctl -q | grep sudo | grep COMMAND | wc -l)
 
-
-#usar wall no debian
-echo "	#Architecture: $ARC
+wall "	#Architecture: $ARC
 	#CPU : $CPU
 	#VCPU : $VCPU
 	#Memory Usage: $USAGE_MEM/${FULL_MEM}MB ($USAGE_MEM_PERCENT%)
